43608f9755aeb72dfb21001b27b79c2d87b6d882
SenseiRands <nicotrajano1022@gmail.com>
2024-09-12 14:33:32 +0800

    add multiplication to calculator

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -6,5 +6,8 @@ public static double add(double a, double b){
 public static double subtract(double a, double b) {
     return a - b;
 }
+public static double multiply(double a, double b) {
+    return a * b;
+}
 
 }
