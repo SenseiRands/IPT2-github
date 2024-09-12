d845fc385a31431aaad616bd38d50eb43268783b
SenseiRands <nicotrajano1022@gmail.com>
2024-09-12 14:35:24 +0800

    add division to calculator

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -9,5 +9,11 @@ public static double subtract(double a, double b) {
 public static double multiply(double a, double b) {
     return a * b;
 }
+public static double divide(double a, double b) {
+    if (b == 0) {
+        throw new ArithmeticException("Division by zero is not allowed");
+    }
+    return a/b;
+}
 
 }
