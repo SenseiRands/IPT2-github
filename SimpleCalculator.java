2e1434da83d4027cba47934ae5f8bd2aaa768cf1
SenseiRands <nicotrajano1022@gmail.com>
2024-09-12 14:52:42 +0800

    add results to calculator

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -21,4 +21,16 @@ public static double modulus(double a, double b) {
     }
     return a % b;
 }
+
+public static void main(String[] args) {
+    double num1 = 10;
+    double num2 = 5;
+
+    System.out.println("Addition: " + add(num1, num2));
+    System.out.println("Subtraction: " + subtract(num1, num2));
+    System.out.println("Multiplication: " + multiply(num1, num2));
+    System.out.println("Division: " + divide(num1, num2));
+    System.out.println("Modulus: " + modulus(num1, num2));
+
+}
 }
